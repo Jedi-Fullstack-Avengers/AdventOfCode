c453f3c2194e3e61c8d2864a3a4bebe8ffc9d95e
rubenofen <rubenofen@gmail.com>
2019-12-02 17:04:47 +0100

    trash code

--- 2/player.ruben.js
+++ 2/player.ruben.js
@@ -1,1 +1,24 @@
-module.exports = {};
+const suma = value1 => value2 => value1 + value2;
+const multiply = value1 => value2 => value1 * value2;
+const calculate = operation => trash => value1 => value2 => position =>
+  operation === 1 ? suma(value1)(value2) : multiply(value1)(value2);
+
+const part1 = input => {
+  let theCalculate = calculate(input[0]);
+  let theResult = 0;
+  const copyInput = [...input];
+  copyInput[1] = 12;
+  copyInput[2] = 2;
+  copyInput.forEach((curr, index, array) => {
+    theCalculate = theCalculate(array[curr]);
+    if (typeof theCalculate !== 'function') {
+      array[curr] = theCalculate;
+      if (array[index + 1] === 99) theResult = array[0];
+      theCalculate = calculate(array[index + 1]);
+    }
+  });
+
+  return theResult;
+};
+
+module.exports = { part1 };
