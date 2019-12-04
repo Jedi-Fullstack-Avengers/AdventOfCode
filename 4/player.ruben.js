f9d22fd9eda9cb9e069c46ebc99002edfd5c3c01
rubenofen <rubenofen@gmail.com>
2019-12-04 23:37:17 +0100

    dia 4 part 2

--- 4/player.ruben.js
+++ 4/player.ruben.js
@@ -6,6 +6,13 @@ const hasSameAdjacentDigit = number =>
     (acc, cur, idx, array) => (acc ? acc : cur === array[idx + 1]),
     false
   );
+
+const hasSameAdjacentDigitStrict = number =>
+  number
+    .toString()
+    .match(/(\d)\1*/g)
+    .reduce((acc, cur) => (acc ? acc : cur.length === 2), false);
+
 const isCrescendo = number =>
   Array.from(number.toString()).every((cur, idx, array) => !(array[idx - 1] > cur));
 
@@ -22,7 +29,15 @@ const part1 = input => {
 };
 
 const part2 = input => {
-  return 0;
+  let result;
+  range(input[0], input[1] - input[0])
+    .pipe(
+      filter(hasSameAdjacentDigitStrict),
+      filter(isCrescendo),
+      reduce(acc => acc + 1, 0)
+    )
+    .subscribe(x => (result = x));
+  return result;
 };
 
 module.exports = { part1, part2 };
