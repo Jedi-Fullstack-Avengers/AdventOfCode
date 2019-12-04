d0d7c8c254c46b90a5acdf94e44d5b151e9adbbc
merabyte <57664098+merabyte@users.noreply.github.com>
2019-12-04 09:53:12 +0100

    dia3

--- 3/player.raul.js
+++ 3/player.raul.js
@@ -2,13 +2,15 @@ module.exports = {
 	part1: data => {
 		const wire1 = {path: data[0], x: 0, y: 0, sibling: null, map: {}};
 		const wire2 = {path: data[1], x: 0, y: 0, sibling: null, map: {}};
-		//const crossings = []; 
-		let closest = null;
+		const crossings = {}; 
+		let closest = null, fewest = null;
 
 		wire1.sibling = wire2;
 		wire2.sibling = wire1;
 		
 		[wire1, wire2].forEach(wire => {
+			let steps = 0;
+			
 			wire.path.forEach(str => {
 				const direction = str[0];
 				const cells = Number(str.substr(1));
@@ -18,21 +20,33 @@ module.exports = {
 				for (let i=0; i<cells; i++) {
 					wire.x += incX;
 					wire.y += incY;
+					steps++;
 					
 					const coords = wire.x + "," + wire.y;
+					
+					wire.map[coords] = steps;
 					
 					if (wire.sibling.map[coords]) {
 						const distance = Math.abs(wire.x) + Math.abs(wire.y);
 						
-						//crossings.push({x: wire.x, y: wire.y});
+						if (!crossings[coords]) {
+							crossings[coords] = steps + wire.sibling.map[coords];
+							
+							if (fewest === null || fewest > crossings[coords]) {
+								fewest = crossings[coords];
+							}
+						}												
+						
 						if (closest === null || closest > distance) closest = distance;					
 					}					
-					
-					wire.map[coords] = true;
 				}
 			});			
 		});
 		
-		return closest;
+		//console.log(Object.keys(crossings).length);
+		//console.log(Object.keys(wire1.map).length);
+		//console.log(Object.keys(wire1.map).length);
+		//console.log("FEWEST = ", fewest);
+		return `CLOSEST: ${closest}, FEWEST: ${fewest}`;
 	}
 };
