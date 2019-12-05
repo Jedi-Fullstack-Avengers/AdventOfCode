ccfd2784a5cc89199b26ca11cc7ccce901fa019a
KoshiroKun <45243062+KoshiroKun@users.noreply.github.com>
2019-12-05 23:30:32 +0100

    remove console.logs

--- 5/player.dasta.js
+++ 5/player.dasta.js
@@ -1,44 +1,44 @@
 const opCodeSum = (store, values, position) => {
-  console.log(`Sum ${values[0]}+${values[1]} on ${position}`);
+  //console.log(`Sum ${values[0]}+${values[1]} on ${position}`);
   store[position] = values[0] + values[1];
   return store;
 };
 
 const opCodeMul = (store, values, position) => {
-  console.log(`Mul ${values[0]}*${values[1]} on ${position}`);
+  //console.log(`Mul ${values[0]}*${values[1]} on ${position}`);
   store[position] = values[0] * values[1];
   return store;
 };
 
 const opCodeSave = (store, value, position) => {
-  console.log(`Save ${value} on ${position}`);
+  //console.log(`Save ${value} on ${position}`);
   store[position] = value;
   return store;
 };
 
 const opCodeLoad = (store, value) => {
-  console.log(`Load ${value} from ${value}`);
+  //console.log(`Load ${value} from ${value}`);
   return store[value];
 };
 
 const jumpIfTrue = values => {
-  console.log(`Try to jump with ${values[0]} != 0`);
+  //console.log(`Try to jump with ${values[0]} != 0`);
   if (values[0] != 0) return values[1];
 };
 
 const jumpIfFalse = values => {
-  console.log(`Try to jump with ${values[0]} == 0`);
+  //console.log(`Try to jump with ${values[0]} == 0`);
   if (values[0] == 0) return values[1];
 };
 
 const lessThan = (store, values, position) => {
   store[position] = values[0] < values[1] ? 1 : 0;
-  console.log(`LessThan ${values[0] < values[1]} on ${position} found ${store[position]}`);
+  //console.log(`LessThan ${values[0] < values[1]} on ${position} found ${store[position]}`);
 };
 
 const equals = (store, values, position) => {
   store[position] = values[0] == values[1] ? 1 : 0;
-  console.log(`Equals ${values[0] == values[1]} on ${position} found ${store[position]}`);
+  //console.log(`Equals ${values[0] == values[1]} on ${position} found ${store[position]}`);
 };
 
 const getOperation = value => {
@@ -93,12 +93,12 @@ const part2 = (input, userInput = 5) => {
     const operation = getOperation(parseInt(intCode[index]));
     if (operation[0] == 9 && operation[1] == 9) break;
     const opCode = parseInt(operation[0]);
-    console.log('TCL: opCode', opCode);
+    //console.log('TCL: opCode', opCode);
     const modParams = [
       parseInt(operation[2] === undefined ? 0 : operation[2]),
       parseInt(operation[3] === undefined ? 0 : operation[3])
     ];
-    console.log('TCL: modParams', modParams);
+    //console.log('TCL: modParams', modParams);
 
     const firstValuePos = intCode[index + 1];
     const secondValuePos = intCode[index + 2];
@@ -106,7 +106,7 @@ const part2 = (input, userInput = 5) => {
     const secondValue = modParams[1] === 0 ? intCode[secondValuePos] : secondValuePos;
     const resultPosition = intCode[index + 3];
 
-    console.log(`Using values: ${firstValue}|${secondValue}`);
+    //console.log(`Using values: ${firstValue}|${secondValue}`);
 
     if (opCode === 1) opCodeSum(intCode, [firstValue, secondValue], resultPosition);
     else if (opCode === 2) opCodeMul(intCode, [firstValue, secondValue], resultPosition);
