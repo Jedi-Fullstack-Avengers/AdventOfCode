fca6761b22a982b57b85e8804d91e368d8b9c40e
dastag <45243062+dastag@users.noreply.github.com>
2019-12-11 12:34:11 +0100

    dasta day 10  part 2 - template day 11

--- 10/player.dasta.js
+++ 10/player.dasta.js
@@ -1,7 +1,9 @@
 const calculateSlope = (origin, target) => {
   return {
-    ang: Math.atan2(origin.x - target.x, origin.y - target.y),
-    slope: reduce(target.y - origin.y, target.x - origin.x).join()
+    ang: Math.atan2(target.x - origin.x, target.y - origin.y),
+    slope: reduce(target.y - origin.y, target.x - origin.x).join(),
+    position: [target.x, target.y],
+    value: [origin.x - target.x, origin.y - target.y]
   };
 };
 
@@ -16,38 +18,77 @@ const sonar = (map, coordinates) => {
 
   map.forEach((row, y) => {
     Array.from(row).forEach((element, x) => {
-      if (element === '#' && (x != coordinates.x || y != coordinates.y)) {
+      if (element === '#' && (x !== coordinates.x || y !== coordinates.y)) {
         const vector = calculateSlope(coordinates, { x, y });
+
         if (
-          !vectors.some(
-            registeredVector =>
-              registeredVector.ang === vector.ang && registeredVector.slope === vector.slope
-          )
-        )
+          !vectors.some((registeredVector, index) => {
+            if (
+              registeredVector.ang === vector.ang &&
+              registeredVector.slope === vector.slope &&
+              Math.abs(registeredVector.value[0]) + Math.abs(registeredVector.value[1]) >
+                Math.abs(vector.value[0]) + Math.abs(vector.value[1])
+            ) {
+              vectors.splice(index, 1, vector);
+            }
+            return registeredVector.ang === vector.ang && registeredVector.slope === vector.slope;
+          })
+        ) {
           vectors.push(vector);
-      }
-    });
-  });
-
-  return vectors.length;
-};
-
-const part1 = input => {
-  let maxMatches = 0;
-  input.forEach((row, y) => {
-    Array.from(row).forEach((element, x) => {
-      if (element === '#') {
-        const matches = sonar(input, { x, y });
-        if (matches > maxMatches) {
-          maxMatches = matches;
         }
       }
     });
   });
 
-  return maxMatches;
+  vectors.sort((a, b) => {
+    return b.ang - a.ang;
+  });
+
+  return vectors;
 };
 
-const part2 = input => {};
+const part1 = (input, part2 = false) => {
+  let maxMatches = { position: '', matches: 0, targets: [] };
+  input.forEach((row, y) => {
+    Array.from(row).forEach((element, x) => {
+      if (element === '#') {
+        const matches = sonar(input, { x, y });
+        if (matches.length > maxMatches.matches) {
+          maxMatches.matches = matches.length;
+          maxMatches.position = `${x},${y}`;
+          maxMatches.targets = matches;
+        }
+      }
+    });
+  });
+
+  return part2 ? maxMatches : maxMatches.matches;
+};
+
+const laser = (map, targets) => {
+  targets.forEach(target => {
+    const shoot = Array.from(map[target.position[1]]);
+    shoot[target.position[0]] = '.';
+    map[target.position[1]] = shoot.join('');
+  });
+
+  return map;
+};
+
+const part2 = input => {
+  let shootList = [];
+  const baseStation = part1(input, true);
+  let updatedMap = laser(input, baseStation.targets);
+  shootList = [...shootList, ...baseStation.targets];
+  let updatedTargets = [];
+
+  do {
+    updatedTargets = sonar(updatedMap, baseStation.position);
+    updatedMap = laser(input, updatedTargets);
+    shootList = [...shootList, ...updatedTargets];
+  } while (updatedTargets.length > 0);
+
+  return shootList[199].position[0] * 100 + shootList[199].position[1];
+};
 
 module.exports = { part1, part2 };
